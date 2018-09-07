5ed0c1833ad26847ace145b4c54009517d5b0463
Mike Sizov <sizov.m.m@gmail.com>
2018-09-07 10:50:15 +0700

    Fix colon symbol handling

--- src/client.js
+++ src/client.js
@@ -167,7 +167,7 @@
                             var custom = null;
                             var $key = $(this.config.keyTemplate);
                             var text = key.length > 1?key.replace(/[\{\}]/gm, ''):key;
-                            var parts = text.split(':');
+                            var parts = (text==":")?[":"]:text.split(':');
                             var modifier = { mod: null, applied: [] };
                             if ( parts.length > 1 ) {
                                 text = parts[0];
